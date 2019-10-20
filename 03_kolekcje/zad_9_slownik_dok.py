9af64d84d50b098832c1b966aa452244283b1bfa
DominikaJastrzebska <d.jastrzebska@sysbreakers.com>
2019-10-20 12:39:37 +0200

    zadania

--- 03_kolekcje/zad_9_slownik_dok.py
+++ 03_kolekcje/zad_9_slownik_dok.py
@@ -6,10 +6,10 @@ przedmioty malymi, drukowanymi lub zaczynajac od duzej litery)
 '''
 
 user_list = []
-for user in range(1, 3): #zmienic na range(1, 6)
+for user in range(1, 6): #zmienic na range(1, 6)
     subject_list = []
     print(('Uzytkownik %d podaje przedmioty: ') %(user))
-    for sub in range(1, 3):
+    for sub in range(1, 5):
         subject = input('Podaj przedmiot: ')
         subject_list.append(subject)
     user_list.append(subject_list)
@@ -24,9 +24,6 @@ for subject_list in user_list:
             subject_dict[subject.lower()] += 1
 print(subject_dict)
 
-for items in subject_dict:
-    print(subject_dict.items())
-    if subject_dict.values() == max(subject_dict.values()):
-        print(subject_dict.items())
-        print(max(subject_dict.values()))
-print(max(subject_dict.values()))
+for key, value in subject_dict.items():
+    if value == max(subject_dict.values()):
+        print('Najpopularniejszy przedmiot to: ', key, 'Wystepuje: ', value, 'razy.')
