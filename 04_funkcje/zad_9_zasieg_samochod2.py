48e6ea20c0b42d932fb103cd36784b943ae19ba5
DominikaJastrzebska <d.jastrzebska@sysbreakers.com>
2019-10-24 15:56:52 +0200

    zadanie samochod

--- 04_funkcje/zad_9_zasieg_samochod2.py
+++ 04_funkcje/zad_9_zasieg_samochod2.py
@@ -34,40 +34,27 @@ jego oryginalności. Dopisz odpowiednie komunikaty.
 # if_old()
 # if_original()
 
-my_car = {'marka': 'Fiat', 'model': '126p', 'rocznik': 1000, 'czy_oryginalny': True}
 
+my_car = {'marka': 'Fiat', 'model': '126p', 'rocznik': 1999, 'czy_oryginalny': True}
 
-def display_my_car():
-    print(my_car)
+is_origin = False
+age = 2019 - my_car['rocznik']
 
-
-def is_old():
-    if 2019 - my_car['rocznik'] >= 25:
-        #lat = 2019 - my_car['rocznik']
-        #print(f'Samochod ma {lat}')
-        return True
-    else:
-        return False
-        #lata = 2019 - my_car['rocznik']
-        #print(f'Samochod ma za malo {lata}')
-
-
-def is_antique(is_original, is_old1):
-    if not is_original:
-        print('Samochod nie moze byc zarejestrowany jako zabytkowy')
-    if not is_old1:
-        print('Samochod moze byc zarejestrowany jako zabytkowy.')
-
-
-
-could_be_original = False
-if my_car['czy_oryginalny'] is True:
-    print('Samochod pretenduje na zabytkowy ze wzgledu na oryginalne czesci.')
-    could_be_original = True
+if age < 25:
+    print('Samochod za mlody')
 else:
-    print('Samochod nie ma nim. 75% oryginalnych czesci, nie moze byc uznany za zabytkowy.')
+    if my_car['czy_oryginalny'] is True:
+        print('Samochod zabytkowy')
+        is_origin = True
+    else:
+        print('Brak oryginalnych czesci')
 
 
-display_my_car()
-isold1 = is_old()
-is_antique(could_be_original, isold1)
+def is_antique(is_original, is_old):
+    if not is_original:
+        return False
+    if is_old >= 25:
+        return True
+
+
+is_antique(is_origin, age)
