81d08fee7e8bb19b510439e577a8e0ebc5be54d1
DominikaJastrzebska <d.jastrzebska@sysbreakers.com>
2020-01-21 22:59:58 +0100

    add checks same el in row and col

--- 05_christmas_tree/tic_tac_toe.py
+++ 05_christmas_tree/tic_tac_toe.py
@@ -7,127 +7,155 @@ Stwórz gre w kółko i Krzyżyk dla 2 graczy. Zacznij od najważniejszej częś
 Przykładowa gra mogłaby wyglądać ten sposób:
 '''
 
-
-def create_board():
-    """
-    Function create empty board of tic tac toe game
-    :return:
-    print board
-    """
-    print('A', '  B  ', 'C', end=' ')
-    print()
-    for i in range(3):
-        for j in range(3):
-            if j < 2:
-                print('. | ', end='')
-            else:
-                print('.')
-
-
 game_board = {
     'A1': '.', 'A2': '.', 'A3': '.',
     'B1': '.', 'B2': '.', 'B3': '.',
     'C1': '.', 'C2': '.', 'C3': '.'
 }
 
+matrix_board = [
+    [' ', 'A ', ' B ', ' C'],
+    [1, '.', '.', '.', ],
+    [2, '.', '.', '.', ],
+    [3, '.', '.', '.', ]
+]
 
-def print_game_board(board):
-    print('  ', 'A', '  B  ', 'C')
-    print(1, f' {board["A1"]} | {board["B1"]} | {board["C1"]}')
-    print(2, f' {board["A2"]} | {board["B2"]} | {board["C2"]}')
-    print(3, f' {board["A3"]} | {board["B3"]} | {board["C3"]}')
-
-
-# def print_game_board_2():
-#     BOARD_ROWS = [' ', 'A', 'B', 'C']
-#     BOARD_COLUMNS = [' ', 1, 2, 3]
-#     board = []
-#     for row_el in BOARD_ROWS:
-#         board_row = []
-#         for col_el in BOARD_COLUMNS:
-#             if board[0]:
-#                 board_row.append(row_el)
-#             else:
-#                 board_row.append('.')
-#         board.append(board_row)
-#     return board
-#
-#
-# print(print_game_board_2())
+dict_row = {'A': 1, 'B': 2, 'C': 3}
 
 
-# rozgrywka
-# turn = 'X'
-# while '.' in game_board.values():
-#     print()
-#     move = input(f'Player {turn} mark position: ')
-#     if game_board[move] != '.':
-#         print('Put the mark on the other position.')
-#     else:
-#         game_board[move] = turn
-#         if turn == 'X':
-#             turn = 'O'
-#         else:
-#             turn = 'X'
-#         print_game_board(game_board)
-#     # win_row(game_board)
-
-
-def tic_tac_toe_matrix():
+def print_game_board(matrix_board):
     """
-    Function create tic tac toe matrix on the model of print_game_board(board) function
+    Function prints game board on the model of matrix board 4x4
+    :param matrix_board: matrix 4x4
     :return:
+    prints matrix board in game
     """
-    tic_tac_toe_matrix = []
-    for row in range(3):
-        matrix_row = []
-        for col in range(3):
-            matrix_row.append('.')
+    # print('  ', 'A', '  B  ', 'C')
+    # print(1, f' {board["A1"]} | {board["B1"]} | {board["C1"]}')
+    # print(2, f' {board["A2"]} | {board["B2"]} | {board["C2"]}')
+    # print(3, f' {board["A3"]} | {board["B3"]} | {board["C3"]}')
 
-        tic_tac_toe_matrix.append(matrix_row)
-    return tic_tac_toe_matrix
-
-
-print('tic tac toe:', tic_tac_toe_matrix())
-
-
-dict_col = {'A': 0, 'B': 1, 'C': 2}
-dict_row = {1: 0, 2: 1, 3: 2}
-
-print()
+    for row in range(len(matrix_board)):
+        print()
+        for col in range(len(matrix_board[row])):
+            if 1 <= row <= 4 and 1 <= col <= 2:
+                print(matrix_board[row][col], end=' | ')
+            else:
+                print(matrix_board[row][col], end=' ')
+    print()
 
 
-def row_wins(matrix):
+def create_matrix_board_3x3(matrix_board):
+    """
+    Function create matrix board 3x3 from matrix board 4x4 by removing first row and column
+    :param matrix_board: matrix 4x4
+    :return: matrix 3x3
+    """
+    new_matrix = [row[1:] for row in matrix_board[1:]]
+    return new_matrix
+
+
+def same_elements_in_row(matrix):
+    """
+    Function checks is row has the sme elements
+    :param matrix: matrix 3x3
+    :return: list (number of row, True or False, first element in row)
+    """
     list_of_true_false = []
     for row in range(3):
         list_of_true_false.append((row, all(x == matrix[row][0] for x in matrix[row]), matrix[row][0]))
     return list_of_true_false
 
 
-def col_wins(matrix):
-    list_true_false = []
+def who_wins_row_col(list_of_true_false):
+    """
+    Function returns element, that won the game
+    :param list_of_true_false:
+    :return: string 'X' or 'O'
+    """
+    for element in list_of_true_false:
+        if element[1] is True:
+            if element[2] != '.':
+                return element[2]
+            else:
+                continue
+
+
+def change_rows_to_col(matrix):
+    """
+    Function changes rows to columns in matrix 3x3
+    :param matrix: matrix 3x3
+    :return: matrix 3x3
+    """
+    # for row in range(3):
+    #     print()
+    #     for col in range(3):
+    #     #     list_true_false.append(col, all(x == matrix[col][0]) for x in martix[col])
+    #         print(matrix[col][row], end=' ')
+    # print()
+
+    matrix_rows_to_col = []
     for row in range(3):
-        print()
+        matrix_rows_to_col_row = []
         for col in range(3):
-        #     list_true_false.append(col, all(x == matrix[col][0]) for x in martix[col])
-            print(matrix[col][row], end=' ')
+            matrix_rows_to_col_row.append(matrix[col][row])
+        matrix_rows_to_col.append(matrix_rows_to_col_row)
+    return matrix_rows_to_col
+
+
+def same_elements_in_col(matrix):
+    """
+    Function checks if in column of matrix are the same elements
+    :param matrix: matrix 3x3
+    :return: list (number of column, True or False, first element in row)
+    """
+    matrix_change = change_rows_to_col(matrix)
+    return same_elements_in_row(matrix_change)
 
 
 def main():
-    print('Create board: ')
-    create_board()
-    print('Print game board')
-    print_game_board(game_board)
-    print('Print tic tac toe matrix on the model of print_game_board(game_board) function')
-    print(tic_tac_toe_matrix())
-    for row in range(3):
-        print()
-        for col in range(3):
-            print(tic_tac_toe_matrix[row][col], end=' ')
+    dict_row = {'A': 1, 'B': 2, 'C': 3}
 
-    matrix_example = [['X', '.', 'X'], ['v', 'v', 'v'], ['.', 'm', '.']]
-    print(row_wins(matrix_example))
-    col_wins(matrix_example)
+    print('Print game board')
+    print_game_board(matrix_board)
+    print()
+    matrix_board_3x3 = create_matrix_board_3x3(matrix_board)
+
+    turn = 'X'
+    while '.' in game_board.values():
+        print()
+        move = input(f'Player {turn} mark position: ')
+        if game_board[move] != '.':
+        # if matrix_board_3x3[int(move[1])][dict_row[move[0]]] != '.':
+            print('Put the mark on the other position.')
+        else:
+            game_board[move] = turn
+            matrix_board[int(move[1])][dict_row[move[0]]] = turn
+            if turn == 'X':
+                turn = 'O'
+            else:
+                turn = 'X'
+            print('print_game_board: ')
+            print_game_board(matrix_board)
+            matrix_board_3x3 = create_matrix_board_3x3(matrix_board)
+            print(matrix_board_3x3)
+            same_el_rows = same_elements_in_row(matrix_board_3x3)
+            same_el_col = same_elements_in_col(matrix_board_3x3)
+            if who_wins_row_col(same_el_rows) is not None:
+                print('And the winner is:', who_wins_row_col(same_el_rows))
+                break
+            elif who_wins_row_col(same_el_col) is not None:
+                print('And the winner is:', who_wins_row_col(same_el_col))
+                break
+
+    print()
+    matrix_example = [['X', '.', 'X'], ['.', '.', '.'], ['O', 'O', 'O']]
+    print(matrix_example)
+    same_elements_row = same_elements_in_row(matrix_board_3x3)
+    print(same_elements_in_row(matrix_example))
+    print(change_rows_to_col(matrix_example))
+    print('matrix board 3x3', matrix_board_3x3)
+    who_wins_row_col(matrix_board_3x3)
 
 
 if __name__ == '__main__':
--- 05_christmas_tree/tic_tac_toe_2.py
+++ 05_christmas_tree/tic_tac_toe_2.py
@@ -1,24 +1,150 @@
-def create_board():
-    # print('  A', '  B  ', 'C', end=' ')
-    print()
-    for i in range(0, 4):
-        if i == 0:
-            print('  A', '  B  ', 'C', end=' ')
-            print()
-        else:
-            print(i, end=' ')
-        for j in range(3):
-            if i == 0:
-                pass
+# def create_board():
+#     # print('  A', '  B  ', 'C', end=' ')
+#     print()
+#     for i in range(0, 4):
+#         if i == 0:
+#             print(' ', 'A  ', 'B  ', 'C', end=' ')
+#             print()
+#         else:
+#             print(i, end=' ')
+#         for j in range(3):
+#             if i == 0:
+#                 pass
+#             else:
+#                 if j < 2:
+#                     print('. | ', end='')
+#                 else:
+#                     print('.')
+#
+#
+# def who_wins():
+#     pass
+#
+#
+# create_board()
+#
+#
+# def transform_move_to_read_matrix_value(move):
+#     two = []
+#     for i in range(len(move)):
+#         two.append(move[i])
+#
+#     return two
+#
+# print(transform_move_to_read_matrix_value('A1'))
+#
+# matrix_board = [
+#     [' ', 'A ', ' B ', ' C'],
+#     [1, '.', '.', '.', ],
+#     [2, '.', '.', '.', ],
+#     [3, '.', '.', '.', ]
+# ]
+#
+# dict_row = {'A': 1, 'B': 2, 'C': 3}
+# turn = 'B2'
+#
+# matrix_board[1][1] = 'x'
+# matrix_board[dict_row[turn[0]]][int(turn[1])] = 'y'
+#
+# print(matrix_board)
+# print('------------')
+#
+#
+# matrix_board_2 = [
+#     [' ', 'A ', ' B ', ' C'],
+#     [1, '.', '.', '.', ],
+#     [2, '.', '.', '.', ],
+#     [3, '.', '.', '.', ]
+# ]
+#
+# new_matrix = [row[1:] for row in matrix_board_2[1:]]
+# print(new_matrix)
+
+# def create_board():
+#     """
+#     Function create empty board of tic tac toe game
+#     :return:
+#     print board
+#     """
+#     # print('A', '  B  ', 'C', end=' ')
+#     # print()
+#     # for i in range(3):
+#     #     for j in range(3):
+#     #         if j < 2:
+#     #             print('. | ', end='')
+#     #         else:
+#     #             print('.')
+#     # print('  A', '  B  ', 'C', end=' ')
+#
+#     for i in range(0, 4):
+#         if i == 0:
+#             print(' ', 'A  ', 'B  ', 'C', end=' ')
+#             print()
+#         else:
+#             print(i, end=' ')
+#         for j in range(3):
+#             if i == 0:
+#                 pass
+#             else:
+#                 if j < 2:
+#                     print('. | ', end='')
+#                 else:
+#                     print('.')
+
+
+
+matrix_board_3x3 = [['X', 'O', 'X'], ['O', 'X', 'O'], ['X', 'O', 'X']]
+
+
+def same_elements_in_row(matrix):
+    list_of_true_false = []
+    for row in range(3):
+        list_of_true_false.append((row, all(x == matrix[row][0] for x in matrix[row]), matrix[row][0]))
+    return list_of_true_false
+
+
+def who_wins_row_col(list_of_true_false):
+    for element in list_of_true_false:
+        # print(element[1])
+        if element[1] is True:
+            if element[2] != '.':
+                return element[2]
             else:
-                if j < 2:
-                    print('. | ', end='')
-                else:
-                    print('.')
+                continue
 
 
-def who_wins():
-    pass
+def change_rows_to_col(matrix):
+    # for row in range(3):
+    #     print()
+    #     for col in range(3):
+    #     #     list_true_false.append(col, all(x == matrix[col][0]) for x in martix[col])
+    #         print(matrix[col][row], end=' ')
+    # print()
+
+    matrix_rows_to_col = []
+    for row in range(3):
+        matrix_rows_to_col_row = []
+        for col in range(3):
+            matrix_rows_to_col_row.append(matrix[col][row])
+        matrix_rows_to_col.append(matrix_rows_to_col_row)
+    return matrix_rows_to_col
 
 
-create_board()
+def same_elements_in_col(matrix):
+    matrix_change = change_rows_to_col(matrix)
+    print(matrix_change)
+    print(same_elements_in_row(matrix_change))
+    return same_elements_in_row(matrix_change)
+
+
+def main():
+    matrix_board_3x3 = [['X', 'O', 'X'], ['X', 'X', 'O'], ['X', 'O', 'X']]
+    same_el_row = same_elements_in_row(matrix_board_3x3)
+    print(same_el_row)
+    print(who_wins_row_col(same_el_row))
+    same_el_col = same_elements_in_col(matrix_board_3x3)
+    print(same_el_col)
+    print(who_wins_row_col(same_el_col))
+
+if __name__ == '__main__':
+    main()
